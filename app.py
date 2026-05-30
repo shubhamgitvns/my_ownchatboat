9b720ccac831e531db52d031c8c214e5ea518ba0
shubhamgitvns <shubhammaurya10092002@gmail.com>
2026-05-30 21:06:21 +0530

    add requrements.txt file

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ from chatboat import get_bot_response
 
 app = Flask(__name__)
 
+if __name__ == "__main__":
+    app.run(host="0.0.0.0",debug=True)
+
 chat_history = [
      {
         # "user": "",
